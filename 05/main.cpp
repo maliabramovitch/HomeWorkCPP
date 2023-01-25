8e1644f52c1360141c3c24522c8f92a4a9130f77
maliabramovitch <107685274+maliabramovitch@users.noreply.github.com>
2023-01-25 20:17:13 +0200

    f

--- 05/main.cpp
+++ 05/main.cpp
@@ -1,10 +1,31 @@
-//
-// Created by Mali Abramovitch on 25/01/2023.
-//
-#include "DirectedGraph.h"
-#include <iosstream>
+#include "MyException.h"
+#include "Neverland.h"
+using namespace std;
 
-int main() {
 
-    return 0;
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        exit(-2);
+    }
+    string out = argv[argc-1];
+    if (out.substr(0, 4) != "[-o]") {
+        out = "output.dat";
+    }
+    else {
+        out = out.substr(4, out.size());
+        --argc;
+    }
+    Neverland* nl = new Neverland();
+    for (int i = 1; i < argc; ++i) {
+        try {
+            nl->initTransportLine(argv[i]);
+        }
+        catch (MyException e) {
+            e.printMessage();
+            continue;
+        }
+    }
+    nl->getLinesInfo(out);
+    delete nl;
+return 0;
 }
